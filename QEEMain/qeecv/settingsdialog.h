6fc72030abe3b653831de94f155e857d8e406766
momantang <momantang@163.com>
2022-03-10 11:31:09 +0800

    参考（拷贝）例子 simpletreemodel

--- QEEMain/qeecv/settingsdialog.h
+++ QEEMain/qeecv/settingsdialog.h
@@ -1,6 +1,6 @@
 #ifndef SETTINGSDIALOG_H
 #define SETTINGSDIALOG_H
-
+#include <QString>
 #include <QDialog>
 #include <QSettings>
 #include <QSharedPointer>
@@ -8,18 +8,60 @@
 #include <qstackedwidget.h>
 #include <QTreeView>
 #include <QtWidgets>
+
+#include <QVariant>
+#include <QList>
+
+#include <QAbstractItemModel>
+#include <QModelIndex>
+
+///
+///参考 Qt6.2.3 中的例子 simpletreemodel
+/// 
+
+
+class TreeItem {
+public:
+	explicit TreeItem(const QList<QVariant>& data, TreeItem* parentItem = nullptr);
+	~TreeItem();
+
+	void appendChild(TreeItem* child);
+	TreeItem* child(int row);
+	int childCount()const;
+	int columnCount() const;
+	QVariant data(int column) const;
+	int row()const;
+	TreeItem* parentItem();
+private:
+	QList<TreeItem*> m_childItems;
+	QList<QVariant> m_itemData;
+	TreeItem* m_parentItem;
+};
+class TreeModel :public QAbstractItemModel {
+	Q_OBJECT
+public:
+	explicit TreeModel(const QString& data, QObject* parent = nullptr);
+	~TreeModel();
+	QVariant data(const QModelIndex& index,int role) const override;
+private:
+	void setModelData(const QStringList& lines, TreeItem* parent);
+	TreeItem* rootItem;
+};
+
+
+
 class SettingsDialog : public QDialog
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit SettingsDialog(QWidget *parent = nullptr);
-    ~SettingsDialog();
+	explicit SettingsDialog(QWidget* parent = nullptr);
+	~SettingsDialog();
 
 private:
-    QListWidget* listWidget;
-    QStackedWidget* stack;
-    QTreeView* treeView;
+	QListWidget* listWidget;
+	QStackedWidget* stack;
+	QTreeView* treeView;
 
 };
 

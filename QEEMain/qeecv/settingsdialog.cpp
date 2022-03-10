570fad3a3457371c7e493d6f0892dabef816fb25
momantang <momantang@163.com>
2022-03-10 17:05:16 +0800

    太复杂了

--- QEEMain/qeecv/settingsdialog.cpp
+++ QEEMain/qeecv/settingsdialog.cpp
@@ -1,14 +1,132 @@
 #include "settingsdialog.h"
 
-SettingsDialog::SettingsDialog(QWidget *parent) :
+
+
+
+
+TreeItem::TreeItem(const QList<QVariant>& data, TreeItem* parentItem):m_itemData(data),m_parentItem(parentItem)
+{
+}
+
+TreeItem::~TreeItem()
+{
+    qDeleteAll(m_childItems);
+}
+
+void TreeItem::appendChild(TreeItem* child)
+{
+    m_childItems.append(child);
+}
+
+TreeItem* TreeItem::child(int row)
+{
+    if (row < 0 || row >= m_childItems.size())
+        return nullptr;
+    return m_childItems.at(row);
+}
+
+int TreeItem::childCount() const
+{
+    return m_childItems.count();
+}
+
+int TreeItem::columnCount() const
+{
+    return m_itemData.count();
+}
+
+QVariant TreeItem::data(int column) const
+{
+    if (column < 0 || column >= m_itemData.size())
+    return QVariant();
+    return m_itemData.at(column);
+}
+
+int TreeItem::row() const
+{
+    if (m_parentItem)
+        return m_parentItem->m_childItems.indexOf(const_cast<TreeItem*>(this));
+
+    return 0;
+}
+
+TreeItem* TreeItem::parentItem()
+{
+    return m_parentItem;
+}
+
+TreeModel::TreeModel(const QString& data, QObject* parent):QAbstractItemModel(parent)
+{
+    rootItem = new TreeItem({ tr("title") });
+    setupModelData(data.split("\n"), rootItem);
+}
+
+TreeModel::~TreeModel()
+{
+    delete rootItem;
+}
+
+Qt::ItemFlags TreeModel::flags(const QModelIndex& index) const
+{
+    return Qt::ItemFlags();
+}
+
+QVariant TreeModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Horizontal && role == Qt::DisplayRole)
+        return rootItem->data(section);
+
+    return QVariant();
+}
+
+QVariant TreeModel::data(const QModelIndex& index, int role) const
+{
+    return QVariant();
+}
+
+QModelIndex TreeModel::index(int row, int column, const QModelIndex& parent) const
+{
+    return QModelIndex();
+}
+
+QModelIndex TreeModel::parent(const QModelIndex& child) const
+{
+    return QModelIndex();
+}
+
+int TreeModel::rowCount(const QModelIndex& parent) const
+{
+    return 0;
+}
+
+int TreeModel::columnCount(const QModelIndex& parent) const
+{
+    return 0;
+}
+
+void TreeModel::setupModelData(const QStringList& lines, TreeItem* parent)
+{
+    QList<TreeItem*>parents;
+    QList<int> indentations;
+    parents << parent;
+    indentations << 0;
+
+    int number = 0;
+
+}
+
+SettingsDialog::SettingsDialog(QWidget* parent) :
     QDialog(parent)
 {
     treeView = new QTreeView;
     treeView->setMinimumWidth(200);
     treeView->setMaximumWidth(200);
     stack = new QStackedWidget;
-    
+
     QHBoxLayout* mainLayout = new QHBoxLayout;
+    QFile file("D:/Qt/Examples/Qt-6.2.3/widgets/itemviews/simpletreemodel/default.txt");
+    TreeModel model(file.readAll());
+    treeView->setModel(&model);
     mainLayout->addWidget(treeView);
     mainLayout->addWidget(stack);
 
@@ -19,5 +137,4 @@ SettingsDialog::SettingsDialog(QWidget *parent) :
 
 SettingsDialog::~SettingsDialog()
 {
-   
 }
--- QEEMain/qeecv/settingsdialog.h
+++ QEEMain/qeecv/settingsdialog.h
@@ -42,10 +42,29 @@ class TreeModel :public QAbstractItemModel {
 public:
 	explicit TreeModel(const QString& data, QObject* parent = nullptr);
 	~TreeModel();
-	QVariant data(const QModelIndex& index,int role) const override;
+	Qt::ItemFlags flags(const QModelIndex& index) const override;
+	QVariant headerData(int section, Qt::Orientation orientation,
+		int role = Qt::DisplayRole) const override;
+
+	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+	QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const override;
+
+	QModelIndex parent(const QModelIndex& child) const override;
+
+	int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+
+	int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
 private:
-	void setModelData(const QStringList& lines, TreeItem* parent);
+	void setupModelData(const QStringList& lines, TreeItem* parent);
 	TreeItem* rootItem;
+
+
+
+
+
+
+
 };
 
 
